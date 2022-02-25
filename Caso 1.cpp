36747934b170af66aaabe8f6bc7a9ff3b93b0fb7
MariLau-RE <marilaure29@estudiantec.cr>
2022-02-24 22:37:29 -0600

    Triplets

---
+++ Caso 1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//Compare The triplets 
+// Dos Algoritmos 
+//The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+
+//The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+
+//If a[i] > b[i], then Alice is awarded 1 point.
+//If a[i] < b[i], then Bob is awarded 1 point.
+//If a[i] = b[i], then neither person receives a point.
+
+void Triplets1(){
+	
+#define SIZE 3
+
+int resultado[]={0,0};
+	
+}
+
+
+int main(){
+  
+  
+  return 0;
+}
