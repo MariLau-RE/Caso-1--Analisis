74a5b509adf8de08d60f0a0d5ac6183bd79601a1
MariLau-RE <marilaure29@estudiantec.cr>
2022-02-27 17:21:08 -0600

    Time conversion

--- Caso 1.cpp
+++ Caso 1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 //Compare The triplets 
@@ -16,12 +17,60 @@ void Triplets1(){
 #define SIZE 3
 
 int resultado[]={0,0};
+int a[]={20,28,70};
+int b[]={50,60,50};
+int comparador;
+int temporal=0;
+
+
+for(int temporal=0; temporal<SIZE; temporal++){
 	
+comparador=(a[temporal] - b[temporal]);
+
+if (comparador>0){
+	
+	resultado[0]=resultado[0]+1;
+	}
+if (comparador<0){
+
+	resultado[1]=resultado[1]+1;
+	
+	}
 }
+cout << " Resultado: "<<"Ana:"<< resultado[0] << " , "<<"Bob:"<< resultado[1] << endl;
+return ;
+
+}
+
+string TimeConversion1(string time){
+
+string minseg=time.substr(2,6);
+int hora= stoi(time.substr(0,2));
+//string AMPM= time[8];
+if (char& time(int 8) == "A")	{
+	if(hora=12){
+		hora=0;
+	}
+	else{	
+	hora=hora+12;
+}
+}
+time= to_string(hora)+minseg;
+return time;
+cout<<"Hora Militar"<<time<<endl;
+}
+	
+
 
 
 int main(){
+	
+
+
+	//Triplets1();
+	TimeConversion1("12:41:15AM");
   
   
   return 0;
 }
+
